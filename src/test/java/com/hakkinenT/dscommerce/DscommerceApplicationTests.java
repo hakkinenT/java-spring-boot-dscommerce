ac26accf969a88c92b1e77db040cf0eab2f2661d
Tawanna Hakkinen <thol_16@hotmail.com>
2023-06-22 14:16:42 -0300

    Project created

---
+++ src/test/java/com/hakkinenT/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
