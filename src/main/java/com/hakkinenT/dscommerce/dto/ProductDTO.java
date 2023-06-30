93ed88da3382ed4d96e12b2a9f95d9522da84b84
Tawanna Hakkinen <thol_16@hotmail.com>
2023-06-30 15:42:54 -0300

    Bean Validation

--- src/main/java/com/hakkinenT/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/hakkinenT/dscommerce/dto/ProductDTO.java
@@ -1,11 +1,21 @@
 package com.hakkinenT.dscommerce.dto;
 
 import com.hakkinenT.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @NotBlank(message = "Campo requerido.")
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
     private String name;
+
+    @NotBlank(message = "Campo requerido.")
+    @Size(min = 10, message = "Descrição preciso ter no mínimo 10 caracteres.")
     private String description;
+
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
