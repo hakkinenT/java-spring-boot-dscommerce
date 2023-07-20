e3a86af49234a67fada6f4f610f26f9c5142ff9e
Tawanna Hakkinen <thol_16@hotmail.com>
2023-07-20 14:17:29 -0300

    UserDetails, GrantedAuthority

--- src/main/java/com/hakkinenT/dscommerce/entities/User.java
+++ src/main/java/com/hakkinenT/dscommerce/entities/User.java
@@ -1,13 +1,15 @@
 package com.hakkinenT.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -81,6 +83,11 @@ public class User {
         this.birthDate = birthDate;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -110,6 +117,33 @@ public class User {
 
         return false;
     }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+
 
 
     @Override
