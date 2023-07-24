f758c06b722a767c99232b71ca65f685e95ed98c
Tawanna Hakkinen <thol_16@hotmail.com>
2023-07-24 15:40:53 -0300

    Fix validation

--- src/main/java/com/hakkinenT/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/hakkinenT/dscommerce/dto/ProductDTO.java
@@ -2,10 +2,7 @@ package com.hakkinenT.dscommerce.dto;
 
 import com.hakkinenT.dscommerce.entities.Category;
 import com.hakkinenT.dscommerce.entities.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +17,7 @@ public class ProductDTO {
     @Size(min = 10, message = "Descrição preciso ter no mínimo 10 caracteres.")
     private String description;
 
+    @NotNull(message = "Campo requerido")
     @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
