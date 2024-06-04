ea63a008da81b63083eef30b6c324ec255ae0c9e
Tawanna <thol_16@hotmail.com>
2024-06-04 20:50:12 -0300

    ProductController improvement in swagger documentation

--- src/main/java/com/hakkinenT/dscommerce/controllers/ProductController.java
+++ src/main/java/com/hakkinenT/dscommerce/controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.hakkinenT.dscommerce.controllers;
 import com.hakkinenT.dscommerce.dto.ProductDTO;
 import com.hakkinenT.dscommerce.dto.ProductMinDTO;
 import com.hakkinenT.dscommerce.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,17 +20,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
+@Tag(name = "Products", description = "Controller for Product")
 public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping(value = "/{id}")
+    @Operation(
+            description = "Get product by id",
+            summary = "Get product by id",
+            responses = {
+                    @ApiResponse(description = "Ok", responseCode = "200"),
+                    @ApiResponse(description = "Not Found", responseCode = "404")
+            }
+    )
+    @GetMapping(value = "/{id}",  produces = "application/json")
     public ResponseEntity<ProductDTO> findById(@PathVariable  Long id){
         ProductDTO dto = productService.findById(id);
         return ResponseEntity.ok(dto);
     }
 
-    @GetMapping
+    @Operation(
+            description = "Paginated list of all products, being able to filter by name",
+            summary = "Get all products",
+            responses = {
+                    @ApiResponse(description = "OK", responseCode = "200")
+            }
+    )
+    @GetMapping(produces = "application/json")
     public ResponseEntity<Page<ProductMinDTO>> findAll(
             @RequestParam(name = "name", defaultValue = "") String name,
             Pageable pageable){
@@ -35,8 +54,18 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(
+            description = "Create a new product",
+            summary = "Create a new product",
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+            }
+    )
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    @PostMapping
+    @PostMapping(produces = "application/json")
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
         dto = productService.insert(dto);
 
@@ -44,8 +73,19 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @Operation(
+            description = "Update a product",
+            summary = "Update a product",
+            responses = {
+                    @ApiResponse(description = "OK", responseCode = "200"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Not Found", responseCode = "404"),
+                    @ApiResponse(description = "Unprocessable Entity", responseCode = "422")
+            }
+    )
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id,@Valid @RequestBody ProductDTO dto){
         dto = productService.update(id, dto);
 
@@ -53,8 +93,19 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(
+            description = "Delete a product",
+            summary = "Delete a product",
+            responses = {
+                    @ApiResponse(description = "No Content", responseCode = "204"),
+                    @ApiResponse(description = "Bad Request", responseCode = "400"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403"),
+                    @ApiResponse(description = "Not Found", responseCode = "404")
+            }
+    )
     @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         productService.delete(id);
 
