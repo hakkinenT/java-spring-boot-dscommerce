9ad004db859fce348bc5e80c644638ff16b91375
Tawanna <thol_16@hotmail.com>
2024-06-04 20:53:23 -0300

    UserController improvement in swagger documentation

--- src/main/java/com/hakkinenT/dscommerce/controllers/UserController.java
+++ src/main/java/com/hakkinenT/dscommerce/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.hakkinenT.dscommerce.controllers;
 
 import com.hakkinenT.dscommerce.dto.UserDTO;
 import com.hakkinenT.dscommerce.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -11,12 +14,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/users")
+@Tag(name = "Users", description = "Controller for User")
 public class UserController {
     @Autowired
     private UserService service;
 
+    @Operation(
+            description = "Get logged user information",
+            summary = "Get logged user information",
+            responses = {
+                    @ApiResponse(description = "OK", responseCode = "200"),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401"),
+                    @ApiResponse(description = "Forbidden", responseCode = "403")
+            }
+    )
     @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
-    @GetMapping(value = "/me")
+    @GetMapping(value = "/me", produces = "application/json")
     public ResponseEntity<UserDTO> getMe(){
         UserDTO dto = service.getMe();
         return ResponseEntity.ok(dto);
